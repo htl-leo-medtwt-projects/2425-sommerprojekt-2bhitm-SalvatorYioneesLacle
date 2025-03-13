dd9aba609494242023ff160cc059b1b8f4c5bf36
Salvator Yionees Lacle <s.lacle@students.htl-leonding.ac.at>
2025-03-13 07:58:28 +0100

    add navigation

--- project/js/page.js
+++ project/js/page.js
@@ -1,10 +1,18 @@
 function initHTML() {
     let str = `
-    <header>
-        <div><img src="./img/hp-logo.png" alt="hp-logo"></div>
-        <div></div>
-    </header>
+    <div>
+        <div>
+            <nav>
+                <div id="nav-logo">
+                    <img src="./img/logo.png" alt="ExpertShop logo">
+                </div>
+                <div id="nav-btns"></div>
+                <div id="nav-account"></div>
+                <div id="nav-items"></div>
+            </nav>
+        </div>
+    </div>
     `;
     document.getElementsByTagName('body').item(0).innerHTML = str;
 }
-initHTML()
+// initHTML()
