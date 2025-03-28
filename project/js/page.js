9e3025831e7dcc87ea25469d57b64887d75f5704
Salvator Yionees Lacle <s.lacle@students.htl-leonding.ac.at>
2025-03-28 07:17:40 +0100

    add navInit to js, audio file correction

--- project/js/page.js
+++ project/js/page.js
@@ -20,36 +20,34 @@ initPageIcon()
 
 function initNavigationbar() {
     let str = `
-                <div id="nav-logo">
-                    <img src="./img/logo.png" alt="ExpertShop logo">
+            <a id="nav-logo" href="/index.html">
+                <img src="/img/logo.png" alt="ExpertShop logo">
+            </a>
+            <div id="nav-btns">
+                <a href="#btnDiv">
+                    <img src="/img/shopIcon.png" alt="shop icon">
+                    <p>Shop</p>
+                </a>
+                <div>
+                    <img src="/img/partners.png" alt="partners icon">
+                    <p>Partners</p>
                 </div>
-                <div id="nav-btns">
-                    <div>
-                        <img src="./img/shopIcon.png" alt="shop icon">
-                        <p>Shop</p>
-                    </div>
-                    <div>
-                        <img src="./img/partners.png" alt="partners icon">
-                        <p>Partners</p>
-                    </div>
-                    <div>
-                        <img src="./img/check.png" alt="account icon">
-                        <p>Account</p>
-                    </div>
+                <div>
+                    <img src="/img/check.png" alt="account icon">
+                    <p>Account</p>
                 </div>
-                <div id="nav-account">
-                    <!-- <div> -->
-                    <p>Account Name 0123</p>
-                    <img src="./img/accountIcon.png" alt="account icon">
-                    <!-- </div> -->
-                </div>
-                <div id="nav-items">
-                    <img src="./img/shopping-cart.png" alt="shopping cart">
-                </div>
+            </div>
+            <div id="nav-account">
+                <p>Account Name 0123</p>
+                <img src="/img/anonymous.jpg" alt="account icon">
+            </div>
+            <div id="nav-items">
+                <img src="/img/shopping-cart.png" alt="shopping cart">
+            </div>
     `;
     document.getElementsByTagName('nav').item(0).innerHTML = str;
 }
-// initNavigationbar()
+initNavigationbar()
 
 function initAccountState() {
 
