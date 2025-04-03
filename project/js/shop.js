16443ba290d06e8e5683dbd2c97e671f76c9979e
Salvator Yionees Lacle <s.lacle@students.htl-leonding.ac.at>
2025-04-03 15:39:21 +0200

    add min/maxPrice check

--- project/js/shop.js
+++ project/js/shop.js
@@ -2,9 +2,33 @@ function initLaptops() {
     let str = '';
     for (let i = 0; i < 10; i++) {
         str += `
-        <div class="itemBox"></div>
+        <div class="itemBox">
+            <div class="itemImg">
+                <img src="/img/phone.jpg" alt="phone">
+            </div>
+
+            <div class="itemStats">
+                <h2>Samsung</h2>
+            </div>
+        </div>
         `;
     }
     document.getElementById('items-grid').innerHTML = str;
 }
-initLaptops()
+initLaptops();
+
+function checkMinValue() {
+    if (document.getElementById('min-price').value < 0) {
+        document.getElementById('min-price').value = 0;
+    }
+
+    if (document.getElementById('min-price').value > document.getElementById('max-price').value) {
+        document.getElementById('min-price').value = document.getElementById('max-price').value;
+    }
+}
+
+function checkMaxValue() {
+    if (document.getElementById('max-price').value < document.getElementById('min-price').value) {
+        document.getElementById('max-price').value = document.getElementById('min-price').value;
+    }
+}
