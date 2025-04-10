40a1ec7ce2973efccefbbdc5c77a43229ed158db
Salvator Yionees Lacle <s.lacle@students.htl-leonding.ac.at>
2025-04-10 10:07:44 +0200

    rename account-profile.html to account-overview.html

--- project/js/account.js
+++ project/js/account.js
@@ -5,3 +5,11 @@ document.getElementsByTagName('nav').item(0).innerHTML = `
             </a>`;
 }
 initNavigationbar()
+
+function toLogOutScreen() {
+    window.location.href = `/pages/account/account-signout.html`;
+}
+
+function toAccountOverview() {
+    window.location.href = `/pages/account/account-overview.html`;
+}
